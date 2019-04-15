bf8ac79d0c965510530039109a45a65ed63fcdd5
dwayne15 <thomas.d.p@hotmail.com>
2019-04-15 14:42:07 +0100

    change script to try reading individual lines

--- DB/genbank_parser.py
+++ DB/genbank_parser.py
@@ -1,18 +1,36 @@
 #!usr/bin/env python3
+import re
 from re import match, split, IGNORECASE
 
 class genbank_parser:
-    def __init__(self):
-        self.openfile = open(filename, 'r') as file
-        self.line = file.readline()
+    def __init__(self, filename):
+        self.openfile = open(filename, 'r')
 
-    def parse_origin(self, line):
-        sequence = []
-        pattern = re.compile('^\d+.*')
-        while pattern.match(line)
-        sequence += line.split('/s+')
-
+    def parse_origin(self):
+        sequence = ''
+        file = self.openfile.readline()
+        for line in file:
+            if 'ORIGIN' in line:
+                while match('^\d+.*'):
+                    splitted = split('\s+', line)
+                    del splitted[0]
+                    sequence += ''.join(splitted).upper()
+                    break
+                    
         return sequence
+
+
+    def parse_accession(self):
+        accession = []
+        pattern = re.compile('ACCESSION')
+        file = self.openfile.readline()
+        for line in file:
+            if pattern.match(line):
+                accession += line.strip
+
+        return accession
+
+
         
         
 
