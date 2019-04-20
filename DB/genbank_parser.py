f78d26a55115e0dfff45dad740e73833856ea508
dwayne15 <thomas.d.p@hotmail.com>
2019-04-20 12:41:58 +0100

    Added function for finding keyword in file

--- DB/genbank_parser.py
+++ DB/genbank_parser.py
@@ -6,35 +6,47 @@ class genbank_parser:
     def __init__(self, filename):
         self.openfile = open(filename, 'r')
 
-    def parse_origin(self):
+    def get_keyword(self, keyword):
         line = ''
-        line_origin = ''
-        sequence = ''
-
-        while not line.startswith('ORIGIN'):
+        while not line.startswith(keyword):
             line = self.openfile.readline().strip()
-    
-        line_origin = self.openfile.readline().strip()
-        while match('^\d+.*', line_origin):
-            splitted = split('\s+', line_origin)
+        return line
+
+    def parse_accession(self):
+        accession = self.get_keyword('ACCESSION')
+                               
+        return accession
+
+    def parse_features(self):
+        line = self.get_keyword('FEATURES')
+            
+        features = []
+        line = self.openfile.readline()
+        while not 'CDS' in line:
+            line = self.openfile.readline()
+        else:
+            name, location = split('\s+', line.strip())
+            features.append(name)
+
+        return features
+
+    def parse_origin(self):
+        sequence = ''
+        line = self.get_keyword('ORIGIN')
+        line = self.openfile.readline().strip()
+
+        while match('^\d+.*', line):
+            splitted = split('\s+', line)
             del splitted[0]
             sequence += ''.join(splitted).upper()
-            line_origin = self.openfile.readline().strip()
+            line = self.openfile.readline().strip()
                         
         return sequence
 
-    def parse_accession(self):
-        line = ''
-        while not line.startswith('ACCESSION'):
-            line = self.openfile.readline().strip()
-            
-        return line
-
     
-
-
     def close(self):
         self.openfile.close()
+        
         
         
 
