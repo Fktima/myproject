ecf798aadfd6e4bd0930d5044f44bbe9783e022f
Miruna Serian <zcbtser@ucl.ac.uk>
2019-05-05 22:50:34 +0100

    Update dummy.py

--- BL/dummy.py
+++ BL/dummy.py
@@ -1,9 +1,11 @@
 
 def get_gene_entry(query):
-
     if (query in query_search):
         for entry in  gene_entry:
             return entry
+
+def get_all_entries():
+    return gene_entry
 
 gene_entry=[]
 
@@ -12,12 +14,12 @@ entry1 ={
     "protein_name": 'protein1',
     "DNA_seq": 'TGAATTCATATGGAATTCTGCAATTATATTCA',
     "aminoacid_sequence":'IHMEFCNYI',
-    "CDS": [1, 6],
+    "CDS": [5, 16],
     "codon_usage": ' this is a codon sequenc',
     "chromosomal_loc": '18iq',
     "genBank_id": '678'
 }
-gene_entry.append(entry1)
+
 
 entry2 ={
     "accession_code":  '4321',
@@ -28,13 +30,12 @@ entry2 ={
     "chromosomal_loc": '18iq',
     "genBank_id": '876'
 }
+gene_entry.append(entry1)
 gene_entry.append(entry2)
 
 query_search=[]
 for entry in gene_entry:
     query_search.append(entry.get("protein_name", ""))
     query_search.append(entry.get("accession_code", ""))
-    query_search.append(entry.get("genBank_id2",""))
+    query_search.append(entry.get("genBank_id",""))
 
-try1= get_gene_entry('678')
-print(try1)
